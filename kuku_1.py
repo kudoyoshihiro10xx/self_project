d62d00522264f5a0e9536cf873c122d68dfb67cd
Kudo <kudo@example.com>
2019-03-11 08:31:08 +0900

    九九表完成(間隔が揃ってる)

--- kuku_1.py
+++ kuku_1.py
@@ -6,5 +6,13 @@ y = 0
 
 for x in range(1, 10):
     for y in range(1, 10):
-        print(x * y, end=' ')
-    print()
+        print('{0:2d} '.format(x * y), end=' ')
+    print('')
+
+# print(' | 1 2 3 4 5 6 7 8 9')
+# print('--+--------------------------')
+# for y in range(1,10):
+#   print('{0:2d}|'.format(y),end="")
+#   for x in range(1,10):
+#     print('{0:2d} '.format(x*y),end="")
+#   print('')
