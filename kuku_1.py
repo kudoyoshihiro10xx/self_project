10884010c0678683d1812e573465f68f511ced50
Kudo <kudo@example.com>
2019-03-12 00:20:07 +0900

    世界の九九表の出力例１:完

--- kuku_1.py
+++ kuku_1.py
@@ -9,6 +9,19 @@ for x in range(1, 10):
         print('{0:4d}'.format(x * y), end=' ')
     print('')
 
+# 入力した任意の九九表
+
+ccc = int(input("縦の列数を入力して下さい:"))
+ddd = int(input("横の列数を入力して下さい:"))
+
+aaa = 0
+bbb = 0
+
+for aaa in range(1, ccc + 1):
+    for bbb in range(1, ddd + 1):
+        print('{0:4d}'.format(aaa * bbb), end=' ')
+    print('')
+
 # print(' | 1 2 3 4 5 6 7 8 9')
 # print('--+--------------------------')
 # for y in range(1,10):
